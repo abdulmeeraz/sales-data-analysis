4e9913fd7b4912b1bd78f096b0e3ec61b0033d82
unknown <abdulmeeraz@gmail.com>
2026-01-02 19:33:22 +0530

    added category filter

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ def sales_analysis():
     total_salesf = sales_data["Total_sales"].sum()
     print(f"Total Sales : ${total_salesf:.2f}")
 
+    # Calculate average sales per transaction
+    average_sales = sales_data["Total_sales"].mean()
+    print(f"Average Sales per Transaction : ${average_sales:.2f}")
+
+    # Best-selling product
+    best_selling_product = sales_data.groupby("Product")["Total_sales"].sum().idxmax()
+    print(f"Best-Selling Product : {best_selling_product}")
+
+    # Sales by category
+    sales_by_category = sales_data.groupby("Category")["Total_sales"].sum()
+    print("\n--- Sales by Category ---")
+    print(sales_by_category)
+    
+
 
 
 sales_analysis()
